241b702e307bfa5d68fa612e2170140d5f451d23
Jessica Garcia <139712565+JessicaCodesx@users.noreply.github.com>
2025-06-19 16:33:38 +0000

    having db conn issues, yoinked previous db conn files from last assignment

--- app/core/config.php
+++ app/core/config.php
@@ -13,9 +13,9 @@ define('CONTROLLERS', ROOT . DS . 'controllers');
 define('LOGS', ROOT . DS . 'logs');	
 define('FILES', ROOT . DS. 'files');
 
-// ---------------------  NEW DATABASE TABLE -------------------------
-define('DB_HOST',         'yxor6.h.filess.io');
-define('DB_USER',         'COSC4806_pricedoll'); 
-define('DB_PASS',         $_ENV['DB_PASS']);
-define('DB_DATABASE',     'COSC4806_pricedoll');
-define('DB_PORT',         '61000');
+// db credentials 
+define('DB_HOST', 'yxor6.h.filess.io');
+define('DB_USER', 'COSC4806_pricedoll');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', 'COSC4806_pricedoll');
+define('DB_PORT', '61000');
--- app/database.php
+++ app/database.php
@@ -4,11 +4,29 @@
  * 
  */
 
+// creating a PDO connection to the db
 function db_connect() {
-    try { 
-        $dbh = new PDO('mysql:host=' . DB_HOST . ';port='. DB_PORT . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
-        return $dbh;
-    } catch (PDOException $e) {
-        //We should set a global variable here so we know the DB is down
-    }
+  try {
+    // the version I made before with the dsn string being constructed within the pdo constructor was throwing an error. 
+    // fixed by constructing the dsn string outside of the constructor, then passing it in with user and pass. collected     // this profound wisdom from https://www.php.net/manual/en/pdo.construct.php 
+
+    // construct dsn string
+    $dsn = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_DATABASE;
+
+    // create new PDO instance passing dsn, user and pass
+    $dbh = new PDO($dsn, DB_USER, DB_PASS);
+
+    // return db connection if successful 
+    return $dbh;
+  } catch (PDOException $e) {
+    // global var to indiciate db is down
+    global $db_down;
+    $db_down = true;
+
+    //log error msg
+    error_log("Database connection failed :( : " . $e->getMessage());
+
+    // return false to indicate connection failed
+    return false;
+  }
 }
