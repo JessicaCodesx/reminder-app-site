26faa4dcc118dfbe5d46d080e3776dad9e52789b
Jessica Garcia <139712565+JessicaCodesx@users.noreply.github.com>
2025-06-19 16:36:45 +0000

    it wasn't the db conn fault it was my silly sign up page logic

--- app/views/create/index.php
+++ app/views/create/index.php
@@ -1,4 +1,4 @@
-<?php require_once 'app/views/templates/headerPublic.php'?>
+<?php require_once 'app/views/templates/headerPublic.php'; ?>
 <main role="main" class="container">
     <div class="page-header" id="banner">
         <div class="row">
@@ -9,48 +9,41 @@
     </div>
 
     <?php if (isset($_SESSION['signup_success'])): ?>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="alert alert-success" role="alert">
-                    <?= htmlspecialchars($_SESSION['signup_success']) ?>
-                </div>
-            </div>
-        </div>
+        <div class="alert alert-success"><?= htmlspecialchars($_SESSION['signup_success']) ?></div>
         <?php unset($_SESSION['signup_success']); ?>
     <?php endif; ?>
 
-    <?php if (isset($_SESSION['failedAuth']) && $_SESSION['failedAuth'] > 0): ?>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="alert alert-warning" role="alert">
-                    Invalid username or password. Attempts: <?= $_SESSION['failedAuth'] ?>
-                </div>
-            </div>
-        </div>
+    <?php if (isset($_SESSION['signup_error'])): ?>
+        <div class="alert alert-danger"><?= htmlspecialchars($_SESSION['signup_error']) ?></div>
+        <?php unset($_SESSION['signup_error']); ?>
     <?php endif; ?>
 
-<div class="row">
-    <div class="col-sm-auto">
-    <form action="/login/verify" method="post" >
-    <fieldset>
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input required type="text" class="form-control" name="username">
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input required type="password" class="form-control" name="password">
-      </div>
-            <br>
-        <button type="submit" class="btn btn-primary">Sign Up</button>
-    </fieldset>
-    </form> 
-  </div>
-    <div class="row mt-3">
-            <div class="col-lg-12">
-                    <p>Already signed up? <a href="/login">Log in here</a></p>
-            </div>
+    <div class="row">
+        <div class="col-sm-auto">
+            <form action="/create/signup" method="post">
+                <fieldset>
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input required type="text" class="form-control" name="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input required type="password" class="form-control" name="password">
+                    </div>
+                    <div class="form-group">
+                        <label for="confirm_password">Confirm Password</label>
+                        <input required type="password" class="form-control" name="confirm_password">
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Sign Up</button>
+                </fieldset>
+            </form> 
+        </div>
     </div>
-</div>
-
-    <?php require_once 'app/views/templates/footer.php' ?>
+    <div class="row mt-3">
+        <div class="col-lg-12">
+            <p>Already signed up? <a href="/login">Log in here</a></p>
+        </div>
+    </div>
+</main>
+<?php require_once 'app/views/templates/footer.php'; ?>
