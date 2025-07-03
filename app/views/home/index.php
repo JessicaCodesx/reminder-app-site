255f817d0bfb495fead6a3e2743e28db6e0e59e6
Jessica Garcia <139712565+JessicaCodesx@users.noreply.github.com>
2025-07-03 13:57:05 +0000

    added reminder button nav from home page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -12,9 +12,16 @@
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Hey</h1>
+                <h1>Hey <?= $_SESSION['username'] ?? 'there' ?>!</h1>
                 <p class="lead"> <?= date("F jS, Y"); ?></p>
             </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>What would you like to do?</h3>
+            <p><a href="/reminders" class="btn btn-primary">📝 My Reminders</a></p>
         </div>
     </div>
 
